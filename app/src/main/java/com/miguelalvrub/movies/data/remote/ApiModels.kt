184b4ad78ebe9d5447527d42d191b97d538a852d
Miguel Alvarez Rubio <amiguelrubio27@gmail.com>
2022-11-22 20:11:12 +0100

    changed poster:URL to poster:String

--- app/src/main/java/com/miguelalvrub/movies/data/remote/ApiModels.kt
+++ app/src/main/java/com/miguelalvrub/movies/data/remote/ApiModels.kt
@@ -2,4 +2,4 @@ package com.miguelalvrub.movies.data.remote
 
 import java.net.URL
 
-data class MoviesApiModel (val id: String, val title: String, val genre: String, val poster: URL)
+data class MoviesApiModel (val id: String, val title: String, val genre: String, val poster: String)
