d2407d72701f4eabb63aaf8c8c399cae9027128b
Miguel Alvarez Rubio <amiguelrubio27@gmail.com>
2022-11-22 19:36:36 +0100

    changed poster:URL to poster:String

--- app/src/main/java/com/miguelalvrub/movies/domain/Models.kt
+++ app/src/main/java/com/miguelalvrub/movies/domain/Models.kt
@@ -2,4 +2,4 @@ package com.miguelalvrub.movies.domain
 
 import java.net.URL
 
-data class Movie(val id: String, val title: String, val genre: String, val poster: URL)
+data class Movie(val id: String, val title: String, val genre: String, val poster: String)
